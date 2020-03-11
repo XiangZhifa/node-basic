a64aca748dedce0db1a1a5817ec28cae586f0f6b
XiangZhifa <974688383@qq.com>
2020-03-11 13:32:00 +0800

    day2:留言板

---
+++ dayTow/code/app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+http
+    .createServer((req, res) => {
+        const url = req.url;
+        if(url==='/'){
+            fs.readFile('./views/index.html',(err,data)=>{
+                if(err){
+                    res.end('404 Not Found.');
+                    return;
+                }
+                res.end(data);
+            });
+        }
+    })
+    .listen(8888,()=>{
+        console.log('正在监听:http://localhost:8888/');
+    });
