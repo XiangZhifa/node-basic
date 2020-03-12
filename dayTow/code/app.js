47c44654d2d1e99df5b80de4f60559ee3f18df2d
XiangZhifa <974688383@qq.com>
2020-03-12 13:55:03 +0800

    node中简单的表单提交

--- dayTow/code/app.js
+++ dayTow/code/app.js
@@ -1,15 +1,57 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+
+let comments = [{
+    name: '张三1',
+    message: '今天天气真好！',
+    date: new Date().toLocaleString()
+}, {
+    name: '张三2',
+    message: '今天天气真好！',
+    date: new Date().toLocaleString()
+}, {
+    name: '张三3',
+    message: '今天天气真好！',
+    date: new Date().toLocaleString()
+}];
 
 http
     .createServer((req, res) => {
-        const url = req.url;
-        if (url === '/') {
+        let parseObj = url.parse(req.url,true);
+        let urlName = parseObj.pathname;
+        if (urlName === '/') {
             fs.readFile('./views/index.html', (err, data) => {
                 if (err) {
-                    res.end('404 Not Found.');
-                    return;
+                    return fs.readFile('./views/404.html', (err, data) => {
+                        res.end(data);
+                    });
                 }
+                let content = '';
+                comments.forEach(item => {
+                    content += `<div class="card">${item.name}说：${item.message}<span class="dateRight">${item.date}</span></div>`
+                });
+                data = data.toString();
+                data = data.replace('_$Content', content);
+                res.end(data);
+            });
+        } else if (urlName === '/post') {
+            fs.readFile('./views/post.html', (err, data) => {
+                if (err) {
+                    return fs.readFile('./views/404.html', (err, data) => {
+                        res.end(data);
+                    });
+                }
+                res.end(data);
+            });
+        } else if (urlName === '/sendMessage') {
+            comments.push(Object.assign(parseObj.query,{date: new Date().toLocaleString()}));
+            //后端重定向
+            res.statusCode = 302;
+            res.setHeader('Location','/');
+            res.end();
+        } else {
+            fs.readFile('./views/404.html', (err, data) => {
                 res.end(data);
             });
         }
